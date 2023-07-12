fc21d90dabe8b850900ff9a20af500bbdc49b71c
ademx7 <abdidaniel2000@gmail.com>
2023-07-12 09:42:06 -0500

    promesas

---
+++ README.md
@@ -0,0 +1,11 @@
+# ¿Qué sucedio al usar async y await?
+Despues de usar `async/await`  noté que el codigo era mas estructurado, más facil de entender.
+# ¿Qué sucedio al usar el método then()?
+Con `then()` hubo un buen encadenamiento de promesas, pero 
+# ¿Qué diferencias encontraste entre async, await y el método then()?
+async/await simplifica el código, haciéndolo más legible y secuencial, mientras que then() permite un mayor control y encadenamiento de promesas.
+
+
+
+
+
--- index.js
+++ index.js
@@ -8,41 +8,51 @@ const rl = readline.createInterface({
 let tasks = [];
 
 function addTask() {
-  rl.question('Ingrese la descripción de la tarea: ', (description) => {
-    const task = {
-      id: tasks.length + 1,
-      description,
-      completed: false
-    };
-    tasks.push(task);
-    console.log('Tarea añadida exitosamente.');
-    showMenu();
+  return new Promise((resolve, reject) => {
+    rl.question('Ingrese la descripción de la tarea: ', (description) => {
+      if (description.trim() === '') {
+        reject('La descripción de la tarea no puede estar vacía.');
+      } else {
+        const task = {
+          id: tasks.length + 1,
+          description,
+          completed: false
+        };
+        tasks.push(task);
+        console.log('Tarea añadida exitosamente.');
+        resolve();
+      }
+    });
   });
 }
 
 function removeTask() {
-  rl.question('Ingrese el ID de la tarea que desea eliminar: ', (taskId) => {
-    const taskIndex = tasks.findIndex(task => task.id === parseInt(taskId));
-    if (taskIndex !== -1) {
-      tasks.splice(taskIndex, 1);
-      console.log('Tarea eliminada exitosamente.');
-    } else {
-      console.log('No se encontró una tarea con ese ID.');
-    }
-    showMenu();
+  return new Promise((resolve, reject) => {
+    rl.question('Ingrese el ID de la tarea que desea eliminar: ', (taskId) => {
+      const taskIndex = tasks.findIndex(task => task.id === parseInt(taskId));
+      if (taskIndex !== -1) {
+        tasks.splice(taskIndex, 1);
+        console.log('Tarea eliminada exitosamente.');
+        resolve();
+      } else {
+        reject('No se encontró una tarea con ese ID.');
+      }
+    });
   });
 }
 
 function completeTask() {
-  rl.question('Ingrese el ID de la tarea que desea marcar como completada: ', (taskId) => {
-    const task = tasks.find(task => task.id === parseInt(taskId));
-    if (task) {
-      task.completed = true;
-      console.log('Tarea marcada como completada.');
-    } else {
-      console.log('No se encontró una tarea con ese ID.');
-    }
-    showMenu();
+  return new Promise((resolve, reject) => {
+    rl.question('Ingrese el ID de la tarea que desea marcar como completada: ', (taskId) => {
+      const task = tasks.find(task => task.id === parseInt(taskId));
+      if (task) {
+        task.completed = true;
+        console.log('Tarea marcada como completada.');
+        resolve();
+      } else {
+        reject('No se encontró una tarea con ese ID.');
+      }
+    });
   });
 }
 
@@ -56,10 +66,9 @@ function showTasks() {
       console.log(`${task.id}. ${status} ${task.description}`);
     });
   }
-  showMenu();
 }
 
-function showMenu() {
+async function showMenu() {
   console.log('\n--- Lista de tareas ---');
   console.log('1. Añadir tarea');
   console.log('2. Eliminar tarea');
@@ -67,28 +76,42 @@ function showMenu() {
   console.log('4. Ver tareas');
   console.log('5. Salir');
 
-  rl.question('Ingrese el número de la opción deseada: ', (choice) => {
+  try {
+    const choice = await new Promise((resolve) => {
+      rl.question('Ingrese el número de la opción deseada: ', (choice) => {
+        resolve(choice);
+      });
+    });
+
     switch (choice) {
       case '1':
-        addTask();
+        await new Promise((resolve) => setTimeout(resolve, 1800));
+        await addTask().catch((error) => { throw new Error(error); });
         break;
       case '2':
-        removeTask();
+        await new Promise((resolve) => setTimeout(resolve, 1800));
+        await removeTask().catch((error) => { throw new Error(error); });
         break;
       case '3':
-        completeTask();
+        await new Promise((resolve) => setTimeout(resolve, 1800));
+        await completeTask().catch((error) => { throw new Error(error); });
         break;
       case '4':
+        await new Promise((resolve) => setTimeout(resolve, 1800));
         showTasks();
         break;
       case '5':
         rl.close();
         break;
       default:
-        console.log('Opción inválida. Por favor, seleccione un número del menú.');
-        showMenu();
-        break;
+        throw new Error('Opción inválida. Por favor, seleccione un número del menú.');
     }
-  });
+
+    showMenu();
+  } catch (error) {
+    console.log('Error:', error.message);
+    showMenu();
+  }
 }
+
 showMenu();
